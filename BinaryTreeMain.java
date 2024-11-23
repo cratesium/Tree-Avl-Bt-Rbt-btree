bb7012d15ea2df2f237552de939911f38a87dcbe
cratesium <cratesium@gmail.com>
2024-11-23 13:09:39 +0530

    adding all

---
+++ BinaryTreeMain.java
@@ -0,0 +1,24 @@
+package Tree;
+
+public class BinaryTreeMain{
+static class Node{
+    int data ;
+    Node left;
+    Node right;
+
+    public Node(){
+
+        this.data= -1;
+        this.left= null;
+        this.right= null;
+    }
+}
+
+
+
+static class BinaryTree {
+
+    
+}
+
+}
